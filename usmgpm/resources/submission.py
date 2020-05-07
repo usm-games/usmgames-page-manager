b7c536d5ad1872c2ef97e5d00c0c6a0962477933
capaths <pcampaa@gmail.com>
2020-05-07 02:32:16 -0400

    update filter submissions if not evaluated

--- usmgpm/resources/submission.py
+++ usmgpm/resources/submission.py
@@ -19,7 +19,7 @@ submission_parser.add_argument('comment', type=str, required=True)
 submission_parser.add_argument('evidence', type=dict, required=True, action='append')
 
 get_submission_parser = reqparse.RequestParser()
-get_submission_parser.add_argument('evaluated', type=bool, required=False, default=False)
+get_submission_parser.add_argument('not_evaluated', type=bool, required=False, default=False)
 
 
 class ChallengeSubmissionList(Resource):
@@ -35,8 +35,8 @@ class ChallengeSubmissionList(Resource):
         subs_q = Submission.query.filter(Submission.challenge_id == c_id).\
             order_by(Submission.submitted.desc())
         args = get_submission_parser.parse_args()
-        if args['evaluated']:
-            subs_q = subs_q.filter(Submission.approved.isnot(None))
+        if args['not_evaluated']:
+            subs_q = subs_q.filter(Submission.approved.is_(None))
         subs: list = subs_q.all()
         return jsonify(list(map(lambda x: x.json, subs)))
 
@@ -89,8 +89,8 @@ class UserSubmissionList(Resource):
 
         subs_q = Submission.query.filter(Submission.user_id == u_id)
         args = get_submission_parser.parse_args()
-        if args['evaluated']:
-            subs_q = subs_q.filter(Submission.approved.isnot(None))
+        if args['not_evaluated']:
+            subs_q = subs_q.filter(Submission.approved.is_(None))
         subs: list = subs_q.order_by(Submission.submitted.desc()).all()
 
         return jsonify(list(map(lambda x: x.json, subs)))
